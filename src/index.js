a5911394fbf6dbe3c783660fc0326d7e0d9e88f2
RomanShajnoga <shajnogar@gmail.com>
2020-09-30 12:31:50 +0300

    feat: add function min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  
+  return !array?0:
+            array.reduce((acumulator,currentValue)=>{
+                 if(acumulator>currentValue) 
+                    acumulator=currentValue; 
+                    return acumulator;},0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array?0:
+  array.reduce((acumulator,currentValue)=>{
+       if(acumulator<currentValue) 
+          acumulator=currentValue; 
+          return acumulator; },0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array||!array.length?0:
+  array.reduce((acumulator,currentValue,array)=>{
+          acumulator+=currentValue; 
+          return acumulator;
+          },0)
+          /array.length;
 }
